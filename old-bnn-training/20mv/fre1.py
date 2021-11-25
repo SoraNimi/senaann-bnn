9a3469149c30d1230654a9e09e31137684c82ea2
milkjellyan_i <NWXbca~431>
2021-11-25 14:02:33 +0800

    111112111111

---
+++ old-bnn-training/20mv/fre1.py
@@ -0,0 +1,69 @@
+# -*- encoding=utf-8 -*-
+import datetime
+import re
+import linecache
+import numpy as np
+import pandas as pd
+
+
+def deal(listStr, i):
+    # list转dataframe
+    column = 'Voltage';
+    df = pd.DataFrame(listStr, columns=[column])
+
+    # 保存到本地excel
+    IndexFile = 'old-bnn-25mv-L0'+'.xlsx'
+    df.to_excel(IndexFile, index=False)
+
+
+if __name__ == '__main__':
+    dataall = []
+    for i in range(1, 201):
+        indexFile = "dnn" + str(i) + ".mt0"
+        s = linecache.getline(indexFile, 5)
+        #print(s)
+        sub = re.sub(' +', ',', s)
+        strAfter = sub
+        data = strAfter.split(',')
+        #print(data)
+        datalist = data[1:513]
+        #datalist = data[513:1025]
+        #datalist = data[1025: 1537]
+        #print(datalist)
+        dataall = dataall + datalist
+    dataall = np.array(dataall, dtype=float)
+    a = pd.cut(dataall, [0.652, 0.657, 0.662])
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 5mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.647, 0.657, 0.667]
+               )
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 10mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.642, 0.657, 0.672]
+               )
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 15mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.637, 0.657, 0.677]
+               )
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 20mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.632, 0.657, 0.682]
+               )
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 25mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.627, 0.657, 0.687]
+               )
+    # 计算频数
+    b = a.value_counts()
+    print('bias voltage 30mv {:.2%}'.format((b[0] + b[1])/102400))
+    #deal(dataall, i)
---
+++ old-bnn-training/20mv/fre2.py
@@ -0,0 +1,73 @@
+# -*- encoding=utf-8 -*-
+import datetime
+import re
+import linecache
+import numpy as np
+import pandas as pd
+if __name__ == '__main__':
+    dataall = []
+    for i in range(1, 201):
+        indexFile = "dnn" + str(i) + ".mt0"
+        s = linecache.getline(indexFile, 5)
+        #print(s)
+        sub = re.sub(' +', ',', s)
+        strAfter = sub
+        data = strAfter.split(',')
+        #print(data)
+        #datalist = data[1:513]
+        datalist = data[513:1025]
+        #datalist = data[1025: 1537]
+        #print(datalist)
+        dataall = dataall + datalist
+    dataall = np.array(dataall, dtype=float)
+    a = pd.cut(dataall, [0.658,0.663,0.668]
+               )
+    b = a.value_counts()
+#print(b[0])
+#print(b[1])
+#print(b)
+#print(b[0] + b[1])
+    print('bias voltage 5mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.653, 0.663, 0.673]
+           )
+# 计算频数
+    b = a.value_counts()
+#print(b[0])
+#print(b[1])
+#print(b)
+    print('bias voltage 10mv {:.2%}'.format((b[0] + b[1])/102400))
+
+    a = pd.cut(dataall, [0.648,0.663,0.678]
+
+           )
+# 计算频数
+    b = a.value_counts()
+    print('bias voltage 15mv {:.2%}'.format((b[0] + b[1])/102400))
+#print(b[0])
+#print(b[1])
+#print(b)
+
+    a = pd.cut(dataall, [0.643, 0.663, 0.683]
+           )
+# 计算频数
+    b = a.value_counts()
+    print('bias voltage 20mv {:.2%}'.format((b[0] + b[1])/102400))
+#print(b[0])
+#print(b[1])
+#print(b)
+
+    a = pd.cut(dataall, [0.638,0.663,0.688]
+           )
+# 计算频数
+    b = a.value_counts()
+    print('bias voltage 25mv {:.2%}'.format((b[0] + b[1])/102400))
+#print(b[0])
+#print(b[1])
+#print(b)
+
+    a = pd.cut(dataall, [0.633, 0.663, 0.693]
+           )
+# 计算频数
+    b = a.value_counts()
+    print('bias voltage 30mv {:.2%}'.format((b[0] + b[1])/102400))
