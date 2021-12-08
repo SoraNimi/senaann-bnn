84efe41557d1adc2af627358b33cbb68982cfec3
SoraNimi <434624198@qq.com>
2021-12-08 09:27:27 +0800

    -----------------

--- old-bnn-training/0mv/test.py
+++ old-bnn-training/0mv/test.py
@@ -15,7 +15,7 @@ def deal(listStr, i):
     df = pd.DataFrame(listStr, columns=[column])
 
     # 保存到本地excel
-    IndexFile = 'offline-200L2Voltage'+'.xlsx'
+    IndexFile = 'offline-200L0Voltage'+'.xlsx'
     df.to_excel(IndexFile, index=False)
 
 
@@ -29,9 +29,9 @@ if __name__ == '__main__':
         strAfter = sub
         data = strAfter.split(',')
         print(data)
-        #datalist = data[1:513]
+        datalist = data[1:513]
         #datalist = data[513:1025]
-        datalist = data[1025: 1537]
+        #datalist = data[1025: 1537]
         #print(datalist)
         dataall = dataall + datalist
     deal(dataall, i)
